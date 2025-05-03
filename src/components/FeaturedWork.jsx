58882b7787563935a46d57d04619845c28f1b4ec
Adam H <ad.hauck@gmail.com>
2025-05-03 00:31:03 -0700

    Added content to featured work display

--- src/components/FeaturedWork.jsx
+++ src/components/FeaturedWork.jsx
@@ -1,5 +1,6 @@
-const FeaturedWork = ({featuredWorkData}) => {
+import { Link } from 'react-router-dom';
 
+const FeaturedWork = ({featuredWorkData}) => {
   
     const shuffleArray = (array) => {
         for (let i = array.length -1; i > 0; i--) {
@@ -19,10 +20,31 @@ const FeaturedWork = ({featuredWorkData}) => {
     console.log('Featured Items:', featuredItems);
 
 
-
     return (
         <>
-            <p>Featured Work Section</p>
+            <div className='featured-work-container project-display-container'>
+                <p>Featured Work Section</p>
+                {featuredItems.map((project, i) => (
+                    <article key={i} className = 'featured-work-card'>
+                        <Link to={`/work/${project.slug}`} className=''>
+                        <img 
+                            src={project.featured_images['medium_large'].src}
+                            srcSet={project.featured_images['medium_large'].srcSet}
+                            sizes='(max-width: 600px) 100vw, (max-width: 900px) 50vw, 33vw'
+                            alt={project.featured_images['medium_large'].alt} 
+                            className='featured-work-image'
+                        />
+                                            
+                        <h3 className='project-card-title'>{project.title.rendered}</h3>
+                        </Link>
+                    </article>
+
+                ))}
+            </div>
+
+            <div className='all-work-button'>
+                <Link to='/work' className='secondary-button'>View All Work</Link>
+            </div>
         </>
     );
 };
