1bb2d75c8b1fe96ef59412f633548af402866790
Adam H <ad.hauck@gmail.com>
2025-05-15 02:47:29 -0700

    refactor singlepage to remove unneeded div

--- src/pages/SinglePage.jsx
+++ src/pages/SinglePage.jsx
@@ -83,7 +83,6 @@ const SinglePage = () => {
                         </section>
 
                         <section className='project-details-section'>
-                            <div className='project-details'>
                                 <div className='project-overview'>
                                     <h2 className='step-4'>Overview</h2>
                                     {projectData[0].acf.project_description && (
@@ -106,7 +105,6 @@ const SinglePage = () => {
                                         </a>)
                                     }
                                 </div>
-                            </div>
                         </section>
 
                         <section className='project-tools-container'>
