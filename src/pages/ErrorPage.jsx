15f80d76b1926e0a2a8c1725e97eb9a5c4174b37
Adam H <ad.hauck@gmail.com>
2025-06-03 00:29:38 -0700

    error page styling and new image

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line
 import { motion } from 'framer-motion';
 import useSEO from '../hooks/useSEO';
-import errorImageWhite from '../images/error-image-white.jpg'
+import errorImageWhite from '/images/error-image-white.png'
 
 const ErrorPage = () => {
 
@@ -11,14 +11,18 @@ const ErrorPage = () => {
         robots: "noindex, nofollow"
     });
     return (
-        <motion.div
+        <motion.div className='error-page-container'
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 1.5 }}>
-            <div>
-                <h1>Error Page</h1>
-                <img src={errorImageWhite} alt="Error" />
+            <div className='error-page'>
+                <h1>WHOOPS</h1>
+                <p>It seems the page you're looking for does not exist. Please check the URL or return to the homepage.</p>
+                <figure className='error-image'>
+                    <img src={errorImageWhite} alt="Error" />
+                    <figcaption>Does anyone know where this wire goes?</figcaption>
+                </figure>
             </div>
         </motion.div>
     );
